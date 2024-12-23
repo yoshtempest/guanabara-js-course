1ffeb93dd28b83b1063dec644b2dea1d0acad22a
Marcelo <marcelobenjaminrocha100@gmail.com>
2024-12-23 09:54:29 -0300

    new: images folder, ways-to-use.png and incremented Element selectors explanation in js

--- src/js/module-c.js
+++ src/js/module-c.js
@@ -19,15 +19,56 @@ const domTreeHeading = document.createElement('h2');
 domTreeHeading.textContent = 'DOM Tree';
 
 const buttonDomTree = document.createElement('button');
-buttonDomTree.textContent = 'What do you mean by DOM tree';
+buttonDomTree.textContent = 'What do you mean by DOM tree?';
 buttonDomTree.onclick = () => {
     alert('The DOM tree basically starts at the root(window), with everything JavaScript inside it.')
 }
+const elementSelectorsHeading = document.createElement('h2')
+elementSelectorsHeading.textContent = 'Element Selectors';
+
+const paragraphAboutWays = document.createElement('p');
+paragraphAboutWays.textContent = 'there are 5 ways to select one or more elements';
+
+const overlay = document.createElement('div');
+overlay.className = 'overlay';
+
+const modal = document.createElement('div');
+modal.className = 'modal';
+
+const modalImage = document.createElement('img')
+modalImage.src = '../images/ways-to-select.png';
+
+const closeModal = document.createElement('button');
+closeModal.textContent = 'Close';
+
+const buttonShowWays = document.createElement('button');
+buttonShowWays.textContent = 'Show me';
+buttonShowWays.onclick = () => {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+}
+closeModal.onclick = () => {
+    overlay.style.display = 'none';
+    modal.style.display = 'none';
+}
+overlay.onclick = () => {
+    overlay.style.display = 'none';
+    modal.style.display = 'none';
+};
+
+modal.appendChild(modalImage);
+modal.appendChild(closeModal);
 
 moduleC.appendChild(heading);
 moduleC.appendChild(paragraph);
 moduleC.appendChild(buttonWhatIsDom);
 moduleC.appendChild(domTreeHeading);
 moduleC.appendChild(buttonDomTree);
+moduleC.appendChild(elementSelectorsHeading);
+moduleC.appendChild(paragraphAboutWays);
+moduleC.appendChild(buttonShowWays);
+moduleC.appendChild(overlay);
+moduleC.appendChild(modal);
+
 
 body.appendChild(moduleC);

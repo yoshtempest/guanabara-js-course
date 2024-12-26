77b818a872390ac21ee63453e2d55b6afe9ed4b3
Marcelo <marcelobenjaminrocha100@gmail.com>
2024-12-26 09:50:36 -0300

    fix: exchange made of css for javascript

--- src/js/module-c.js
+++ src/js/module-c.js
@@ -11,7 +11,11 @@ paragraph.textContent = 'Dom = Document Object Model'
 
 const buttonWhatIsDom = document.createElement('button');
 buttonWhatIsDom.textContent = 'What is the DOM?';
-buttonWhatIsDom.onclick = () => {
+buttonWhatIsDom.style.backgroundColor = 'cyan';
+buttonWhatIsDom.style.borderRadius = '50px';
+buttonWhatIsDom.style.cursor = 'pointer';
+buttonWhatIsDom.onclick = () =>
+{
     alert('Dom is basically a set of objects inside your browser that will give access to the internal components of your website.');
 }
 
@@ -20,7 +24,11 @@ domTreeHeading.textContent = 'DOM Tree';
 
 const buttonDomTree = document.createElement('button');
 buttonDomTree.textContent = 'What do you mean by DOM tree?';
-buttonDomTree.onclick = () => {
+buttonDomTree.style.backgroundColor = 'cyan';
+buttonDomTree.style.borderRadius = '50px';
+buttonDomTree.style.cursor = 'pointer';
+buttonDomTree.onclick = () =>
+{
     alert('The DOM tree basically starts at the root(window), with everything JavaScript inside it.')
 }
 const elementSelectorsHeading = document.createElement('h2')
@@ -34,24 +42,45 @@ overlay.className = 'overlay';
 
 const modal = document.createElement('div');
 modal.className = 'modal';
+modal.style.backgroundColor = 'gray';
+modal.style.display = 'none';
+modal.style.position = 'fixed';
+modal.style.top = '50%';
+modal.style.left = '50%';
 
 const modalImage = document.createElement('img')
 modalImage.src = '../images/ways-to-select.png';
 
 const closeModal = document.createElement('button');
 closeModal.textContent = 'Close';
+closeModal.style.position = 'relative';
+closeModal.style.right = '40px';
+closeModal.style.top = '0px';
+closeModal.style.width = '40px';
+closeModal.style.height = '25px';
+closeModal.style.backgroundColor = 'red';
+closeModal.style.borderRadius = '10px';
+closeModal.style.cursor = 'pointer';
+closeModal.style.borderColor = 'red';
 
 const buttonShowWays = document.createElement('button');
 buttonShowWays.textContent = 'Show me';
-buttonShowWays.onclick = () => {
-    modal.style.display = 'block';
-    overlay.style.display = 'block';
+buttonShowWays.style.backgroundColor = 'cyan';
+buttonShowWays.style.borderRadius = '50px';
+buttonShowWays.style.cursor = 'pointer';
+
+buttonShowWays.onclick = () =>
+{
+    modal.style.display = 'flex';
+    overlay.style.display = 'flex';
 }
-closeModal.onclick = () => {
+closeModal.onclick = () =>
+{
     overlay.style.display = 'none';
     modal.style.display = 'none';
 }
-overlay.onclick = () => {
+overlay.onclick = () =>
+{
     overlay.style.display = 'none';
     modal.style.display = 'none';
 };
